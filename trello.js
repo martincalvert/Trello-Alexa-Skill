eb06cece48dd5db2ea89d9d4e7d7c6cd11923b91
Calvert <martinlcalvert@gmail.com>
2017-07-13 09:24:03 -0700

    Add posting cards to lists

--- trello.js
+++ trello.js
@@ -10,6 +10,33 @@ const handlers = {
           this.emit(':tell', results['answer']);
       });
     },
+    'AddToCostcoIntent': function () {
+      let card = {
+        name: this.event.request.intent.slots.item.value,
+        idList: '595147301abe1d8b6feff947'
+      }
+      addCard(card, (results) => {
+          this.emit(':tell', results + 'costco');
+      });
+    },
+    'AddToTargetIntent': function () {
+      let card = {
+        name: this.event.request.intent.slots.item.value,
+        idList: '5951473a47a5b8a867a93e98'
+      }
+      addCard(card, (results) => {
+          this.emit(':tell', results + 'target');
+      });
+    },
+    'AddToGroceriesIntent': function () {
+      let card = {
+        name: this.event.request.intent.slots.item.value,
+        idList: '59514735834301079b5db6d4'
+      }
+      addCard(card, (results) => {
+          this.emit(':tell', results + 'groceries');
+      });
+    },
     'AMAZON.HelpIntent': function () {
         this.emit(':ask', "Try are there any cards ready for review");
     },
@@ -22,11 +49,11 @@ const handlers = {
 };
 
 var https = require('https');
+const key = process.env.TRELLO_KEY;
+const token = process.env.TRELLO_TOKEN;
 
 function fetchCards(callback){
-  const key = process.env.TRELLO_KEY;
-  const token = process.env.TRELLO_TOKEN;
-  var options = {
+  let options = {
     host: 'api.trello.com',
     path: '/1/lists/58acf51c8d3d22142a544bc2/cards?key=' + key + '&token=' + token,
     method: 'GET',
@@ -34,15 +61,15 @@ function fetchCards(callback){
       'Content-Type': 'application/json'
     }
   };
-  var retData = '';
-  var ret = {};
-  var req = https.request(options, res => {
+  let retData = '';
+  let ret = {};
+  let req = https.request(options, res => {
     res.on('data', chunk => {
       retData = retData + chunk;
     });
 
     res.on('end', () => {
-        var json = JSON.parse(retData);
+        let json = JSON.parse(retData);
         if (json.length > 0){
           ret['answer'] = 'Yes there is ' + json.length + ' ready for review'
         } else {
@@ -53,6 +80,31 @@ function fetchCards(callback){
     })
   });
 
+  req.end();
+}
+
+function addCard(card, callback){
+  var options = {
+    host: 'api.trello.com',
+    path: '/1/cards?key=' + key + '&token=' + token,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  var retData = '';
+  var ret = {};
+  var req = https.request(options, res => {
+    res.on('data', chunk => {
+      //retData = retData + chunk;
+    });
+
+    res.on('end', () => {
+      callback('Ok added ' + card.name + ' to ');
+    })
+  });
+
+  req.write(JSON.stringify(card));
   req.end();
 }
 
